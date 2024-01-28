b9f38b965f545d05e7b06387438868a695e622ad
BuildTools <unconfigured@null.spigotmc.org>
2024-01-28 16:52:49 +0300

    beta_test

--- board.py
+++ board.py
@@ -29,37 +29,89 @@ class Board:
                 # Отрисовка квадрата
                 pygame.draw.rect(screen, (88,73,73), (x * self.cell_size + self.left,
                                                  y * self.cell_size + self.top,
-                                                 self.cell_size, self.cell_size), 1)
+                                                 self.cell_size, self.cell_size), 2)
                 pygame.draw.rect(screen, color, (x * self.cell_size + self.left + 3,
                                                                     y * self.cell_size + self.top + 3,
                                                                     self.cell_size - 3, self.cell_size - 3))
                 # Отрисовка значения квадрата
                 if value:
-                    font = pygame.font.Font(None, 72)
-                    text_value = font.render(str(value), 1, (255, 255, 255))
-                    screen.blit(text_value, (x * self.cell_size + self.left + 25, y * self.cell_size + self.top + 15))
+                    if len(str(value)) == 1:
+                        font = pygame.font.Font(None, 70)
+                        text_value = font.render(str(value), 1, (255, 255, 255))
+                        screen.blit(text_value,
+                                    (x * self.cell_size + self.left + 26, y * self.cell_size + self.top + 15))
+                    elif len(str(value)) == 2:
+                        font = pygame.font.Font(None, 64)
+                        text_value = font.render(str(value), 1, (255, 255, 255))
+                        screen.blit(text_value,
+                                    (x * self.cell_size + self.left + 17, y * self.cell_size + self.top + 18))
+                    elif len(str(value)) == 3:
+                        font = pygame.font.Font(None, 58)
+                        text_value = font.render(str(value), 1, (255, 255, 255))
+                        screen.blit(text_value,
+                                    (x * self.cell_size + self.left + 6, y * self.cell_size + self.top + 20))
+                    elif len(str(value)) == 4:
+                        font = pygame.font.Font(None, 48)
+                        text_value = font.render(str(value), 1, (255, 255, 255))
+                        screen.blit(text_value,
+                                    (x * self.cell_size + self.left + 3, y * self.cell_size + self.top + 23))
 
     def move(self, direction):
-        if direction == 'right':
-            for y in self.board:
-                # ненулевые квадраты
-                good_value = [value_x for value_x in y if value_x != 0]
-                y[:] = [0] * (self.width - len(good_value)) + good_value
+        if direction == 'left':
+            for row in self.board:
+                while 0 in row:
+                    row.remove(0)
+                while len(row) != self.width:
+                    row.append(0)
+            for y in range(self.height):
+                for x in range(self.width - 1):
+                    if self.board[y][x] == self.board[y][x+1] and self.board[y][x] != 0:
+                        self.board[y][x] *= 2
+                        self.board[y].pop(x+1)
+                        self.board[y].append(0)
 
-                for i in range(self.width - 1, 0, -1):
-                    if y[i] == y[i - 1]:
-                        y[i] *= 2
-                        y[i - 1] = 0
+        elif direction == 'right':
+            for row in self.board:
+                while 0 in row:
+                    row.remove(0)
+                while len(row) != self.width:
+                    row.insert(0, 0)
+            for y in range(self.height):
+                for x in range(self.width - 1, 0, -1):
+                    if self.board[y][x] == self.board[y][x-1] and self.board[y][x] != 0:
+                        self.board[y][x] *= 2
+                        self.board[y].pop(x-1)
+                        self.board[y].insert(0, 0)
 
-        elif direction == 'left':
-            for y in self.board:
-                good_value = [value for value in y if value != 0]
-                y[:] = good_value + [0] * (self.width - len(good_value))
+        if direction == 'up':
+            for x in range(self.width):
+                column = [self.board[y][x] for y in range(self.height)]
+                while 0 in column:
+                    column.remove(0)
+                while len(column) != self.height:
+                    column.append(0)
+                for y in range(self.height - 1):
+                    if column[y] == column[y + 1] and column[y] != 0:
+                        column[y] *= 2
+                        column.pop(y + 1)
+                        column.append(0)
+                for y in range(self.height):
+                    self.board[y][x] = column[y]
 
-                for i in range(self.width - 1):
-                    if y[i] == y[i + 1]:
-                        y[i] *= 2
-                        y[i + 1] = 0
+        elif direction == 'down':
+            for x in range(self.width):
+                column = [self.board[y][x] for y in range(self.height)]
+                while 0 in column:
+                    column.remove(0)
+                while len(column) != self.height:
+                    column.insert(0, 0)
+                for y in range(self.height - 1, 0, -1):
+                    if column[y] == column[y - 1] and column[y] != 0:
+                        column[y] *= 2
+                        column.pop(y - 1)
+                        column.insert(0, 0)
+                for y in range(self.height):
+                    self.board[y][x] = column[y]
 
         self.generate_square()
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+import sys
 from board import Board
 
 pygame.init()
@@ -12,8 +12,51 @@ fps = 60
 font = pygame.font.Font(None, 72)
 title = font.render('3072', 1, (18, 30, 19))
 
+def terminate():
+    pygame.quit()
+    sys.exit()
 
-board = Board(5, 5)
+def check_coords(pos):
+    global board
+    if 271 > pos[0] > 121 and 382 > pos[1] > 232:
+        board = Board(4, 4)
+        return True
+    elif 471 > pos[0] > 321 and 382 > pos[1] > 232:
+        board = Board(5, 5)
+        return True
+    elif 671 > pos[0] > 521 and 382 > pos[1] > 232:
+        board = Board(6, 6)
+        return True
+    return False
+
+def start_screen():
+    font = pygame.font.Font(None, 80)
+    choosing = True
+    board_4 = font.render('4x4', 1, (0, 0, 0))
+    board_5 = font.render('5x5', 1, (0, 0, 0))
+    board_6 = font.render('6x6', 1, (0, 0, 0))
+    font = pygame.font.Font(None, 72)
+    text_tip = font.render('Выбери размер поля', 1, (0, 0, 0))
+    while choosing:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if check_coords(event.pos):
+                    return
+        screen.fill((220, 220, 220))
+        screen.blit(text_tip, (140, 65))
+        for i in range(3):
+            pygame.draw.rect(screen, (223,227,216), (121 + i * 200, 232, 150, 150))
+            pygame.draw.rect(screen, (88, 73, 73), (121 + i*200, 232, 150, 150), 5)
+        screen.blit(board_4, (150, 280))
+        screen.blit(board_5, (350, 280))
+        screen.blit(board_6, (550, 280))
+        pygame.display.flip()
+        clock.tick(60)
+
+start_screen()
+board.generate_square()
 running = True
 while running:
     for event in pygame.event.get():
